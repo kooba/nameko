61d91dbbd3be87749d604f5e9628329eef44a69a
Jakub Borys <jakub.borys@gmail.com>
2015-12-29 17:51:54 +0000

    Initial version of reloader.

--- nameko/cli/run.py
+++ nameko/cli/run.py
@@ -24,6 +24,8 @@ from nameko.exceptions import CommandError
 from nameko.extensions import ENTRYPOINT_EXTENSIONS_ATTR
 from nameko.runners import ServiceRunner
 
+from nameko.cli.reloader import run_with_reloader
+
 
 logger = logging.getLogger(__name__)
 
@@ -115,48 +117,55 @@ def setup_backdoor(runner, port):
 
 
 def run(services, config, backdoor_port=None):
-    service_runner = ServiceRunner(config)
-    for service_cls in services:
-        service_runner.add_service(service_cls)
 
-    def shutdown(signum, frame):
-        # signal handlers are run by the MAINLOOP and cannot use eventlet
-        # primitives, so we have to call `stop` in a greenlet
-        eventlet.spawn_n(service_runner.stop)
+    def run_inn():
 
-    signal.signal(signal.SIGTERM, shutdown)
+        service_runner = ServiceRunner(config)
 
-    if backdoor_port is not None:
-        setup_backdoor(service_runner, backdoor_port)
+        for service_cls in services:
+            service_runner.add_service(service_cls)
 
-    service_runner.start()
+        def shutdown(signum, frame):
+            # signal handlers are run by the MAINLOOP and cannot use eventlet
+            # primitives, so we have to call `stop` in a greenlet
+            eventlet.spawn_n(service_runner.stop)
 
-    # if the signal handler fires while eventlet is waiting on a socket,
-    # the __main__ greenlet gets an OSError(4) "Interrupted system call".
-    # This is a side-effect of the eventlet hub mechanism. To protect nameko
-    # from seeing the exception, we wrap the runner.wait call in a greenlet
-    # spawned here, so that we can catch (and silence) the exception.
-    runnlet = eventlet.spawn(service_runner.wait)
+        signal.signal(signal.SIGTERM, shutdown)
 
-    while True:
-        try:
-            runnlet.wait()
-        except OSError as exc:
-            if exc.errno == errno.EINTR:
-                # this is the OSError(4) caused by the signalhandler.
-                # ignore and go back to waiting on the runner
-                continue
-            raise
-        except KeyboardInterrupt:
-            print()  # looks nicer with the ^C e.g. bash prints in the terminal
+        if backdoor_port is not None:
+            setup_backdoor(service_runner, backdoor_port)
+
+        service_runner.start()
+
+        # if the signal handler fires while eventlet is waiting on a socket,
+        # the __main__ greenlet gets an OSError(4) "Interrupted system call".
+        # This is a side-effect of the eventlet hub mechanism. To protect nameko
+        # from seeing the exception, we wrap the runner.wait call in a greenlet
+        # spawned here, so that we can catch (and silence) the exception.
+        runnlet = eventlet.spawn(service_runner.wait)
+
+
+        while True:
             try:
-                service_runner.stop()
+                runnlet.wait()
+            except OSError as exc:
+                if exc.errno == errno.EINTR:
+                    # this is the OSError(4) caused by the signalhandler.
+                    # ignore and go back to waiting on the runner
+                    continue
+                raise
             except KeyboardInterrupt:
-                print()  # as above
-                service_runner.kill()
-        else:
-            # runner.wait completed
-            break
+                print()  # looks nicer with the ^C e.g. bash prints in the terminal
+                try:
+                    service_runner.stop()
+                except KeyboardInterrupt:
+                    print()  # as above
+                    service_runner.kill()
+            else:
+                # runner.wait completed
+                break
+
+    run_with_reloader(run_inn)
 
 
 def main(args):
